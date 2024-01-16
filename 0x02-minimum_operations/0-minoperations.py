c6d15db296f01ba3195048837c4410d08d6c94a5
belyne <122968087+belyne@users.noreply.github.com>
2024-01-16 15:58:27 +0200

    Update 0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,1 +1,38 @@
+#!/usr/bin/python3
+"""
+Calculates the fewest number of operations needed to result in exactly n H characters in the file.
+"""
+
+def minOperations(n):
+    """
+    Calculates the fewest number of operations needed to result in exactly n H characters in the file.
+
+    Args:
+        n (int): The target number of H characters.
+
+    Returns:
+        int: The fewest number of operations needed. If n is impossible to achieve, return 0.
+    """
+    if n <= 1:
+        return 0
+
+    operations = 0
+    i = 2
+
+    while i <= n:
+        if n % i == 0:
+            n //= i
+            operations += i
+        else:
+            i += 1
+
+    return operations
+
+if __name__ == "__main__":
+    # Test cases
+    n1 = 4
+    print("Min # of operations to reach {} char: {}".format(n1, minOperations(n1)))
+
+    n2 = 12
+    print("Min # of operations to reach {} char: {}".format(n2, minOperations(n2)))
 
