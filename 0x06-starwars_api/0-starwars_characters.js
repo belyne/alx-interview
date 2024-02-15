238b1b5e035d7e0e2ec4cf04e84674bb9cc5f0f1
belyne <nzabonamarie@gmail.com>
2024-02-15 17:14:33 +0000

    starwars_api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -5,33 +5,27 @@ const request = require('request');
 const movieId = process.argv[2];
 const movieEndpoint = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
 
-function fetchAndPrintCharacterNames(characterList, index) {
-  if (index >= characterList.length) {
-    console.log('Finished fetching character names.');
+function sendRequest (characterList, index) {
+  if (characterList.length === index) {
     return;
   }
 
   request(characterList[index], (error, response, body) => {
     if (error) {
-      console.error('Error fetching character data:', error);
+      console.log(error);
     } else {
-      const characterData = JSON.parse(body);
-      console.log('Character Name:', characterData.name);
-      fetchAndPrintCharacterNames(characterList, index + 1);
+      console.log(JSON.parse(body).name);
+      sendRequest(characterList, index + 1);
     }
   });
 }
 
 request(movieEndpoint, (error, response, body) => {
   if (error) {
-    console.error('Error fetching movie data:', error);
+    console.log(error);
   } else {
     const characterList = JSON.parse(body).characters;
 
-    if (!characterList || characterList.length === 0) {
-      console.log('No characters found for this movie.');
-    } else {
-      fetchAndPrintCharacterNames(characterList, 0);
-    }
+    sendRequest(characterList, 0);
   }
 });
