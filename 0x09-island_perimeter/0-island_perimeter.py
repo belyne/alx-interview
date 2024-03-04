7c1e6282fe57255d00653d0fa461704f06601185
belyne <122968087+belyne@users.noreply.github.com>
2024-03-04 08:50:30 +0200

    Update 0-island_perimeter.py

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,1 +1,20 @@
+def island_perimeter(grid):
+    perimeter = 0
 
+    # Iterate through each cell in the grid
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == 1:  # If the cell is part of the island
+                perimeter += 4  # Add 4 to the perimeter
+
+                # Check neighbors and subtract 1 for each adjacent land cell
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 1
+                if i < len(grid) - 1 and grid[i + 1][j] == 1:
+                    perimeter -= 1
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 1
+                if j < len(grid[0]) - 1 and grid[i][j + 1] == 1:
+                    perimeter -= 1
+
+    return perimeter
